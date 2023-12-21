7613d77ae27fcdb9e3fdeb455d660d7bad563a3d
Shepetkov Timerlan <sergeevp284@gmail.com>
2023-12-21 23:00:18 +0300

    get request and return Vacancy sj

--- hh_superjob_api/sj_api.py
+++ hh_superjob_api/sj_api.py
@@ -1,14 +1,33 @@
 from abc_method import VacanciApi
 import requests
+import os
+
+from parsers_hh_superjob.objects.vacancy import Vacancy
 
 
 class SuperJob(VacanciApi):
-    get_params = 'https://api.superjob.ru/2.0/vacancies/'
+    sj_key = os.getenv("Api_sj_key")
 
-    def __init__(self, api_sj: str):
-        self._api_sj = api_sj
+    def __init__(self):
+        self._api_sj = 'https://api.superjob.ru/2.0/vacancies/'
 
-    def get_request(self):
-        return requests.get(self._api_sj)
+    def get_request(self, query_text):
+        response = requests.get(f"{self._api_sj}?keyword={query_text}", headers={'X-Api-App-Id': self.sj_key})
+        if response.status_code == 200:
+            data = response.json()
+            result = []
+            for sj_vacancy in data["objects"]:
+                payment_from = sj_vacancy.get('payment_from', 0)
+                payment_to = sj_vacancy.get('payment_to', 0)
+                #payment = payment_from if payment_from > payment_to else payment_to
+                payment = max(payment_from, payment_to)
+                vacancy = Vacancy(sj_vacancy['profession'], sj_vacancy['link'], payment, sj_vacancy['candidat'])
+                result.append(vacancy)
+            return result
+
+        else:
+            return []
 
 
+sj = SuperJob()
+print(sj.get_request('python'))
